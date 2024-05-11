5b60ecbe2c109cec36b5cb0f3e5ca7e6bda63f5c
martinaghajanian <95177134+martinaghajanian@users.noreply.github.com>
2024-05-11 18:32:11 +0400

    Update CourseArray.java

--- CourseArray.java
+++ CourseArray.java
@@ -101,4 +101,13 @@ public class CourseArray {
 		for (int i = 1; i < elements.length; i++)
 			System.out.println(i + "\t" + elements[i].mySlot);
 	}
+
+	public int[] getTimeSlot(int index) {
+		int pattern[] = new int[181 + 1];
+		for (int i = 1; i < pattern.length; i++) {
+			pattern[i] = slot(i) == index ? 1 : -1;
+		}
+
+		return pattern;
+	}
 }
